353cda601c073d1fba7d40fc2380d9ef35ae5783
barzhsu <iambarz@gmail.com>
2025-02-19 12:40:19 +0800

    update install sop for mac

--- vnpy/install_osx.sh
+++ vnpy/install_osx.sh
@@ -22,8 +22,8 @@ function ta-lib-exists()
 ta-lib-exists || install-ta-lib
 
 # install ta-lib
-$python -m pip install numpy==1.23.1 --index $pypi_index
-$python -m pip install ta-lib==0.4.24 --index $pypi_index
+$python -m pip install numpy --index $pypi_index
+$python -m pip install ta-lib--index $pypi_index
 
 # Install Python Modules
 $python -m pip install -r requirements.txt --index $pypi_index
